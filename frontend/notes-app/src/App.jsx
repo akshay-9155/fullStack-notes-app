c5ba098b0a3941b5827ecf342bd04ca2fdbd3378
akshay-9155 <akshayanand9162@gmail.com>
2024-07-02 04:45:00 +0530

    setup tailwind

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  return <div className="min-h-screen min-w-screen text-zinc-50 bg-zinc-950">
+    Hello
+  </div>;
 }
 
 export default App
